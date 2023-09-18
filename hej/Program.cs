1087cbe350339c89714219272dd3240a3a2f589b
Dino <dino.omicevic@elev.ga.ntig.sem>
2023-09-18 08:44:50 +0200

    hej

---
+++ hej/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+string name = "";
+string favoritesport = "";
+string petname = "";
+
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit sport?");
+favoritesport = Console.ReadLine();
+
+Console.WriteLine("Vad heter ditt husdjur?");
+petname = Console.ReadLine();
+
+Console.WriteLine($"Hej {name} du är bajs på {favoritesport} och {petname} gillar inte dig");
+
+Console.WriteLine("Klicka ENTER för att avsluta");
+Console.ReadLine();
+
+Console.WriteLine($"Hejdå {name}");
+
+Console.ReadLine();
+
+
